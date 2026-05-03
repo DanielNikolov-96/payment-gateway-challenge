e1d797b0bdfc97d580ed8f4d19bd1f4f127d28f4
DanielNikolov-96 <daniel.nikolov@bath.edu>
2026-05-03 21:57:55 +0100

    unused imports

--- src/test/java/com/checkout/payment/gateway/controller/PaymentGatewayControllerTest.java
+++ src/test/java/com/checkout/payment/gateway/controller/PaymentGatewayControllerTest.java
@@ -13,7 +13,6 @@ import com.checkout.payment.gateway.repository.PaymentsRepository;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletResponse;
